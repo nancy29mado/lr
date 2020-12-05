f62d2ebc5e08c665915768e57ca725945547e1e1
nancy29mado <74123898+nancy29mado@users.noreply.github.com>
2020-12-04 18:19:41 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>.: inicio:.</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+			<h2>REGISTRO Y LOGIN </h2>
+             <p>Registrate para ingresar a nuestra página de innovaciones :) </p>
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
